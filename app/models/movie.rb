4f028370e1df4a1976121643ae0b544deaacd5b5
saasbook <saasbook@saasbook.(none)>
2014-02-24 13:54:36 -0400

    Add gettetters/setters to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ActiveRecord::Base
+  attr_accessible :title, :rating, :description, :release_date
+  def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end #shortcut:array of strings 
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 #Rottenpotatoes::Application.config.session_store :cookie_store, key: '_rottenpotatoes_session', :domain=>:all
-Rottenpotatoes::Application.config.session_store :cookie_store, key: '_dyu6hw2_session'
+Rottenpotatoes::Application.config.session_store :cookie_store, key: '_rottenpotatoes_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
